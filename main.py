61f76c34055cc327d589548e242f4540b5a92859
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-08-23 21:24:14 +0530

    Add environment variables

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Hello World')
+import os
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+
+app: FastAPI = FastAPI()
+
+
+@app.get(path='/home/')
+async def home():
+    return {"message": "hello world"}
+
+
+if __name__ == "__main__":
+    DOT_ENV_PATH = os.path.join(os.getcwd(), ".env")
+    load_dotenv(dotenv_path=DOT_ENV_PATH)
+
+    HOST = os.getenv('HOST')
+    PORT = int(os.getenv('PORT'))
+
+    uvicorn.run(
+        app=app, host=HOST, port=PORT,
+        # reload=True
+    )
